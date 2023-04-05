a067154f029a970bbf17104ce0662d800e9852b9
Jprimero15 <jprimero155@gmail.com>
2023-04-05 14:12:47 +0000

    aospa: Improve vanilla build support
    Change-Id: Ib1718be88d20b3c6d360d2b5e8793d0bb7679e20
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -70,6 +70,12 @@ PRODUCT_PACKAGES += \
     ParanoidLauncherOverlay \
     PermissionControllerOverlay
 
+ifeq ($(TARGET_DISABLES_GMS), true)
+# Overlay (Vanilla build)
+PRODUCT_PACKAGES += \
+    VanillaFrameworksOverlay
+endif
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
 
