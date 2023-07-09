c944a2dde19e581071c3c9f9314466959378961d
Yuan Si <do4suki@gmail.com>
2023-07-09 14:18:04 +0800

    products: Add Xiaomi Pad 6
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I6017792d35550253c5e0bbc5f3e94f3e1188bcae

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -46,6 +46,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/panther/aospa_panther.mk \
     $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
     $(LOCAL_DIR)/phone1/aospa_phone1.mk \
+    $(LOCAL_DIR)/pipa/aospa_pipa.mk \
     $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
@@ -87,6 +88,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_panther-userdebug \
     aospa_phoenix-userdebug \
     aospa_phone1-userdebug \
+    aospa_pipa-userdebug \
     aospa_porsche-userdebug \
     aospa_raven-userdebug \
     aospa_spes-userdebug \
