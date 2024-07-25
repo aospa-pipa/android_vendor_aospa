6e4b95242a6b305ff6f61e6cb051b6619f66507b
Omkar Chandorkar <gotenksIN@aospa.co>
2024-07-25 11:58:47 +0900

    aospa: Declare preinstalled packages
    Change-Id: I323951b06c47767933e9527a77df758625f04edd
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -174,6 +174,10 @@ endif
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+# Preinstalled Packages
+PRODUCT_COPY_FILES += \
+    vendor/aospa/target/config/preinstalled-packages-aospa.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-aospa.xml
+
 # Privapp-permissions
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
